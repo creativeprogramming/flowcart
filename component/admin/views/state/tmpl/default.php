df96f65e3f04ea898f4bccb605a92f10ab596c7b
Roberto - phproberto <roberto@phproberto.com>
2012-11-22 02:47:23 +0100

    [imp] extra controllers / views

---
+++ component/admin/views/state/tmpl/default.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * @package     Joomla.Administrator
+ * @subpackage  com_flowcart
+ *
+ * @author      Seth Warburton & Roberto Segura <social@flowcart.me>
+ * @copyright   (c) 2012 Flowcart. All Rights Reserved.
+ * @license     GNU/GPL 2, http://www.gnu.org/licenses/gpl-2.0.htm
+ */
+defined('_JEXEC') or die;
+
+?>
+<h2><?php echo JText::_('STATE FORM GOES HERE!'); ?></h2>
